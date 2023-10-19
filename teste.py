3c655e3a9ad4ee81d204cbce7cb259e86a29197a
HerbieHSSO <54962184+HerbieHSSO@users.noreply.github.com>
2023-10-19 09:00:52 -0300

    Add files via upload

--- teste.py
+++ teste.py
@@ -6,16 +6,14 @@
 
 import serial #Importa a biblioteca
 
-import plotly.graph_objects as go 
+
   
 x = []
 y = []
 # Creating the Figure instance 
-f = go.FigureWidget()  
-  
+
 # showing the plot 
-f
-f.add_scatter(y=y)
+
 while True: #Loop para a conexão com o Arduino
  
     arduino = serial.Serial(port='COM3', baudrate=115200)
@@ -31,7 +29,6 @@ while True: #Loop principal
       
         y.append(int(text.split('contagem: ')[1]))
         # update scatter data
-        scatter = f.data[0]
-        scatter.y = [3, 1, 4, 3]
+
 
 
